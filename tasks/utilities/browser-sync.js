71eed240fcffaf109954c449e38c0feef0af77ae
Maarten van Spil <maarten@insyde.nl>
2016-07-26 16:21:45 +0200

    Fix browser sync path

--- tasks/utilities/browser-sync.js
+++ tasks/utilities/browser-sync.js
@@ -2,10 +2,13 @@
 module.exports = {
 	fn: function (gulp, plugins, options) {
 		var projectPath = __dirname.toLowerCase().replace(options.htdocs, "");
+		projectPath = projectPath.replace("node_modules\\front-end-toolkit\\tasks\\utilities", "");
+		projectPath = projectPath.replace("\\", "/");
+		console.log(projectPath);
 		// Serve files from the root of this project
 		plugins.browserSync.init({
 			open: "external",
-			proxy: "localhost/" + projectPath.replace("\\", "/") + "/site"
+			proxy: "localhost/" + projectPath + "/site"
 		});
 	}
 };
