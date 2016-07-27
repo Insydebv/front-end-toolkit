4e39815cfcf27f7659096202522a22023aee3dc6
Maarten van Spil <maarten@insyde.nl>
2016-07-27 12:03:19 +0200

    Fix bower scripts

--- tasks/bower/scripts.js
+++ tasks/bower/scripts.js
@@ -1,11 +1,10 @@
 // Concat and minify main Bower scripts
 module.exports = {
 	fn: function (gulp, plugins, options) {
-		return gulp.src(plugins.mainBowerFiles(), {base: options.bower.src})
+		return gulp.src(plugins.mainBowerFiles('**/*.js'), {base: options.bower.src})
 			.pipe(plugins.sourcemaps.init())
-			.pipe(plugins.filter('*.js'))
+			.pipe(plugins.babel({}))
 			.pipe(plugins.concat(options.bower.scriptFile))
-			.pipe(plugins.babel())
 			.pipe(plugins.uglify())
 			.pipe(plugins.sourcemaps.write('maps'))
 			.pipe(gulp.dest(options.scripts.dest))
