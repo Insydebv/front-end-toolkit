162bc980b1d989fa15ea84eed44b1eeb90af7f75
Maarten van Spil <maarten@insyde.nl>
2016-07-28 12:21:01 +0200

    Concat script source arrays

--- tasks/lint/scripts.js
+++ tasks/lint/scripts.js
@@ -1,9 +1,9 @@
 // Lint script
 module.exports = {
-  fn: function (gulp, plugins, options) {
-    return gulp.src([options.scripts.bodyScriptSrc, options.scripts.headScriptSrc, options.scripts.pageScriptSrc])
-      .pipe(plugins.jshint())
-      .pipe(plugins.jshint.reporter(plugins.jshintStylish))
-      ;
-  }
+	fn: function (gulp, plugins, options) {
+		return gulp.src(options.scripts.bodyScriptSrc.concat(options.scripts.headScriptSrc, options.scripts.pageScriptSrc))
+			.pipe(plugins.jshint())
+			.pipe(plugins.jshint.reporter(plugins.jshintStylish))
+			;
+	}
 };
