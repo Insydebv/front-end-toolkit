dbab8e28584eba91853eff9bf956d8d5554822c6
Maarten van Spil <maarten@insyde.nl>
2016-07-28 13:05:55 +0200

    Fix styles linter path

--- tasks/lint/styles.js
+++ tasks/lint/styles.js
@@ -2,7 +2,7 @@
 var sassLint = require('gulp-sass-lint');
 module.exports = {
 	fn: function (gulp, plugins, options) {
-		return gulp.src(options.styles.srcFolder + "**/*.scss")
+		return gulp.src(options.styles.srcFolder + "/**/*.scss")
 			.pipe(sassLint({
 				configFile: options.styles.lintConfig,
 				files: {
