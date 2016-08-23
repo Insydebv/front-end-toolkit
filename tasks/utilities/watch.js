dc90e0643fba4502accf6bc3919c38b82b5ddf40
Maarten van Spil <maarten@insyde.nl>
2016-08-23 16:02:00 +0200

    Fix watch task for G4

--- tasks/utilities/watch.js
+++ tasks/utilities/watch.js
@@ -1,20 +1,26 @@
 // // Watch for changes
 module.exports = function (gulp, plugins, options) {
-	gulp.task('utilities:watch', function () {
-		gulp.watch(options.scripts.bodyScriptSrc, ['scripts:bodyscripts']);
-		gulp.watch(options.scripts.headScriptSrc, ['scripts:headscripts']);
-		gulp.watch(options.scripts.pageScriptSrc, ['scripts:pagescripts']);
+	function reload(done) {
+		plugins.browserSync.reload();
+		done();
+	}
 
-		gulp.watch(options.styles.srcFolder + '/**/*.scss', ['styles:build']);
-		gulp.watch(options.styles.componentsSrc + '/**/*.scss', ['styles:build']);
+	gulp.task('utilities:watch', function (done) {
+		gulp.watch(options.scripts.bodyScriptSrc, gulp.series('scripts:bodyscripts'));
+		gulp.watch(options.scripts.headScriptSrc, gulp.series('scripts:headscripts'));
+		gulp.watch(options.scripts.pageScriptSrc, gulp.series('scripts:pagescripts'));
 
-		gulp.watch(options.fonts.src + '/**/*', ['fonts:build']);
-		gulp.watch(options.images.src + '/**/*', ['images:imagemin']);
+		gulp.watch(options.styles.srcFolder + '/**/*.scss', gulp.series('styles:build'));
+		gulp.watch(options.styles.componentsSrc + '/**/*.scss', gulp.series('styles:build'));
+
+		gulp.watch(options.fonts.src + '/**/*', gulp.series('fonts:build'));
+		gulp.watch(options.images.src + '/**/*', gulp.series('images:imagemin'));
 		// gulp.watch('src/images/sprite/**/*', ['sprite']);
 		gulp.watch([
 			'templates/**/*.html',
 			'site/protected/views/**/*.php',
 			'site/protected/widgets/**/*.php',
-		]).on('change', plugins.browserSync.reload);
+		], gulp.series(reload));
+		done();
 	});
 };
