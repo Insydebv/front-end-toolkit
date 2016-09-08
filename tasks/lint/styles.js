3af1705772da356b6253ece025df0923db929c8d
Maarten van Spil <maarten@insyde.nl>
2016-09-08 09:42:41 +0200

    Fix style linter

--- tasks/lint/styles.js
+++ tasks/lint/styles.js
@@ -1,7 +1,7 @@
 // Lint styles
 module.exports = function (gulp, plugins, options, onError) {
 	gulp.task('lint:styles', function () {
-		return gulp.src([options.styles.srcFolder + "/**/*.scss", "!" + options.bower.stylesFile, "!" + options.sprite.cssName], , {since: gulp.lastRun('lint:styles')})
+		return gulp.src([options.styles.srcFolder + "/**/*.scss", "!" + options.bower.stylesFile, "!" + options.sprite.cssName], {since: gulp.lastRun('lint:styles')})
 			.pipe(plugins.sassLint({
 				configFile: options.styles.lintConfig,
 				files: {
