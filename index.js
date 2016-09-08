1cc17d152764727ad5babb14e1f2c0e6f9a20f56
Maarten van Spil <maarten@insyde.nl>
2016-09-08 09:27:09 +0200

    Fix scripts linter and update readme

--- index.js
+++ index.js
@@ -19,29 +19,34 @@ module.exports = function (gulp, options) {
 
 	// Handle errors
 	var jsHintErrorReporter = function (file, enc, callback) {
-		if (file.jshint.success) {
-			// Don't show something if success
-			callback();
-		}
+		if (file.jshint) {
+			if (file.jshint.success) {
+				// Don't show something if success
+				callback(null, file);
+			}
 
-		// Count errors
-		var errorCount = 0;
-		var codes = { W: 0, E: 0 };
-		(file.jshint.results||[]).forEach(function(result) {
-			codes[result.error.code[0]]++;
-		});
-
-		if (!codes['E'] && codes['W']) {
-			// Only warnings, so don't show a notification
-			callback();
-		}
-		else if (codes['E'] > 0) {
-			// There is a true error, so notify!
-			var err = new plugins.util.PluginError('gulp-jshint', {
-				message: 'See console',
+			// Count errors
+			var errorCount = 0;
+			var codes = {W: 0, E: 0};
+			(file.jshint.results || []).forEach(function (result) {
+				codes[result.error.code[0]]++;
 			});
-			this.emit("error", err);
-			callback();
+
+			if (!codes['E'] && codes['W']) {
+				// Only warnings, so don't show a notification
+				callback(null, file);
+			}
+			else if (codes['E'] > 0) {
+				// There is a true error, so notify!
+				var err = new plugins.util.PluginError('gulp-jshint', {
+					message: 'See console',
+				});
+				this.emit("error", err);
+				callback(null, file);
+			}
+		}
+		else {
+			callback(null, file);
 		}
 	};
 
--- tasks/lint/scripts.js
+++ tasks/lint/scripts.js
@@ -3,8 +3,7 @@ var through = require('through2');
 
 module.exports = function (gulp, plugins, options, onError, jsHintErrorReporter) {
 	gulp.task('lint:scripts', function () {
-		return gulp.src(options.scripts.bodyScriptSrc.concat(options.scripts.headScriptSrc, options.scripts.pageScriptSrc))
-			.pipe(plugins.cached('scripts'))
+		return gulp.src(options.scripts.bodyScriptSrc.concat(options.scripts.headScriptSrc, options.scripts.pageScriptSrc), {since: gulp.lastRun('lint:scripts')})
 			.pipe(plugins.plumber({
 				errorHandler: onError
 			}))
