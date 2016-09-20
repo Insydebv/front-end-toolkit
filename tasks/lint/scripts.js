609663e16433ebb9758c996e5db5173318eb3a7f
INSYDE\christian <christian@insyde.nl>
2016-09-20 12:26:27 +0200

    Added ESLint, needs testing!

--- tasks/lint/scripts.js
+++ tasks/lint/scripts.js
@@ -2,15 +2,16 @@
 'use strict';
 const plugins = require('../../libs/plugins');
 const onError = require('../../libs/onError');
-const jsHintErrorReporter = require('../../libs/jsHintErrorReporter');
+const eslint = require('gulp-eslint');
 const through = require('through2');
 
 module.exports = (gulp, options) => () => {
 	return gulp.src(options.scripts.bodyScriptSrc.concat(options.scripts.headScriptSrc, options.scripts.pageScriptSrc), {since: gulp.lastRun('lint:scripts')})
+		.pipe(eslint())
 		.pipe(plugins.plumber({
 			errorHandler: onError
 		}))
-		.pipe(plugins.jshint())
-		.pipe(plugins.jshint.reporter(plugins.jshintStylish))
-		.pipe(through.obj(jsHintErrorReporter));
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError())
+		.on("error", notify.onError('You have JavaScript Errors, check the console!'));
 };
