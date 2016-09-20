96fce7f8d4c4774a381e51d5ca88e93cc4db185f
INSYDE\christian <christian@insyde.nl>
2016-09-20 12:21:55 +0200

    Revert "Added ESLint, needs testing!"
    This reverts commit 75a7c9970a6996f83c91d9837b3679c98636f74d.

--- tasks/lint/scripts.js
+++ tasks/lint/scripts.js
@@ -2,16 +2,15 @@
 'use strict';
 const plugins = require('../../libs/plugins');
 const onError = require('../../libs/onError');
-const eslint = require('gulp-eslint');
+const jsHintErrorReporter = require('../../libs/jsHintErrorReporter');
 const through = require('through2');
 
 module.exports = (gulp, options) => () => {
 	return gulp.src(options.scripts.bodyScriptSrc.concat(options.scripts.headScriptSrc, options.scripts.pageScriptSrc), {since: gulp.lastRun('lint:scripts')})
-		.pipe(eslint())
 		.pipe(plugins.plumber({
 			errorHandler: onError
 		}))
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError())
-		.on("error", notify.onError('You have JavaScript Errors, check the console!'));
+		.pipe(plugins.jshint())
+		.pipe(plugins.jshint.reporter(plugins.jshintStylish))
+		.pipe(through.obj(jsHintErrorReporter));
 };
