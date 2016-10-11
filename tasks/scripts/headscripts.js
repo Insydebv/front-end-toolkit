ee66be01cb0b04cfed88aeabaa3a83d5af106933
INSYDE\christian <christian@insyde.nl>
2016-10-11 16:39:52 +0200

    Removed browserify from the headscripts, seems to cause modernizr problems.

--- tasks/scripts/headscripts.js
+++ tasks/scripts/headscripts.js
@@ -13,10 +13,6 @@ module.exports = (gulp, options) => () => {
 		// Translate code to ES5 (especially useful when ES6 is used)
 		.pipe(plugins.babel())
 		.pipe(plugins.concat(options.scripts.headScriptFile))
-		// Handle the imports used in the code
-		.pipe(plugins.browserify({
-			insertGlobals : true
-		}))
 		// Minify the code
 		.pipe(!plugins.util.env.production ? plugins.util.noop() : plugins.uglify())
 		.pipe(plugins.sourcemaps.write('maps'))
