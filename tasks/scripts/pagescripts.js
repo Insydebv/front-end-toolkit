3f298b70affe91c042004c0ec04d5d8940eba994
INSYDE\christian <christian@insyde.nl>
2016-10-11 12:03:47 +0200

    Fixed merge conflicts

--- tasks/scripts/pagescripts.js
+++ tasks/scripts/pagescripts.js
@@ -2,27 +2,56 @@
 'use strict';
 const plugins = require('../../libs/plugins');
 const onError = require('../../libs/onError');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = (gulp, options) => () => {
-	return gulp.src(options.scripts.pageScriptSrc)
-		.pipe(plugins.plumber({
-			errorHandler: onError
-		}))
-		.pipe(plugins.filter('**/*.js'))
-		.pipe(plugins.sourcemaps.init())
-		// Translate code to ES5 (especially useful when ES6 is used)
-		.pipe(plugins.babel({
-			presets: ['es2015']
-		}))
-		// Handle the imports used in the code
-		.pipe(plugins.browserify({
-			insertGlobals : true
-		}))
-		// Minify the code
-		.pipe(!plugins.util.env.production ? plugins.util.noop() : plugins.babel({
-			presets: ['babili']
-		}))
-		.pipe(plugins.sourcemaps.write('maps'))
-		.pipe(gulp.dest(options.scripts.dest))
+	function getFolders(dir) {
+		return fs.readdirSync(dir)
+			.filter(function(file) {
+				return fs.statSync(path.join(dir, file)).isDirectory();
+			});
+	}
+
+	let folders = getFolders(options.scripts.pageScriptSrc);
+
+	// Parse subfolders
+	let folderStream = folders.map(function(folder) {
+		return gulp.src(path.join(options.scripts.pageScriptSrc, folder, '/**/*.js'))
+			.pipe(plugins.plumber({
+				errorHandler: onError
+			}))
+			.pipe(plugins.sourcemaps.init())
+			// Translate code to ES5 (especially useful when ES6 is used)
+			.pipe(plugins.babel({
+				presets: ['es2015']
+			}))
+			// Handle the imports used in the code
+			.pipe(plugins.browserify({
+				insertGlobals : true
+			}))
+			.pipe(plugins.concat(folder + '.min.js'))
+			// Minify the code
+			.pipe(!plugins.util.env.production ? plugins.util.noop() : plugins.babel({
+				presets: ['babili']
+			}))
+			.pipe(plugins.sourcemaps.write('maps'))
+			.pipe(gulp.dest(options.scripts.dest))
+	});
+
+	// Parse root dir
+	let rootStream = gulp.src(path.join(options.scripts.pageScriptSrc, '/*.js'))
+			.pipe(plugins.plumber({
+				errorHandler: onError
+			}))
+			.pipe(plugins.sourcemaps.init())
+			.pipe(plugins.babel())
+			.pipe(!plugins.util.env.production ? plugins.util.noop() : plugins.uglify())
+			.pipe(plugins.sourcemaps.write('maps'))
+			.pipe(plugins.rename({ suffix: '.min' }))
+			.pipe(gulp.dest(options.scripts.dest))
 		;
+
+	// Return a merged stream to handle both `end` events
+	return plugins.mergeStream(folderStream, rootStream);
 };
