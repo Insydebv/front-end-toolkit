c80ef3ff41131eaacaf82fc2b2bc0e3bee762000
Maarten van Spil <maarten@insyde.nl>
2017-09-20 16:26:54 +0200

    Remove notify from linter

--- tasks/lint/scripts.js
+++ tasks/lint/scripts.js
@@ -11,16 +11,5 @@ module.exports = (gulp, options) => () => {
     .pipe(plugins.plumber())
     .pipe(plugins.eslint())
     .pipe(plugins.eslint.format())
-    // Manually call notifier with the gulp icon to prevent breaking the stream
-    .pipe(plugins.eslint.results(results => {
-      if (results.errorCount) {
-        notifier.notify({
-          icon: path.join(__dirname, '../../../gulp-notify', 'assets', 'gulp-error.png'),
-          title: 'Task Failed [gulp-eslint]',
-          message: 'See console',
-          sound: 'Beep'
-        });
-      }
-    }))
     .pipe(plugins.util.env.production ? plugins.eslint.failAfterError() : plugins.util.noop());
 };
