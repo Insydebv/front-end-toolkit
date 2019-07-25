7e509fe8afd8a31a262e3b19b351b61c2320ff4f
Rens Heddes <rens.heddes@insyde.nl>
2019-07-25 15:47:59 +0200

    Update watch tasks to new syntax

--- tasks/utilities/watch.js
+++ tasks/utilities/watch.js
@@ -8,41 +8,41 @@ module.exports = (gulp, options) => () => {
 
 	// Reload browser after tasks are finished
 	// This will be easier in gulp 4.x.x
-  gulp.task('watchScripts', ['lint:scripts'], function (done) {
+  gulp.task('watchScripts', gulp.series('lint:scripts'), function (done) {
     plugins.browserSync.reload();
     done();
   });
-  gulp.task('watchVendorScripts', ['steal:build'], function (done) {
+  gulp.task('watchVendorScripts', gulp.series('steal:build'), function (done) {
     plugins.browserSync.reload();
     done();
   });
-	gulp.task('watchFont', ['fonts:build'], function (done) {
+	gulp.task('watchFont', gulp.series('fonts:build'), function (done) {
 		plugins.browserSync.reload();
 		done();
 	});
-	gulp.task('watchImages', ['images:imagemin'], function (done) {
+	gulp.task('watchImages', gulp.series('images:imagemin'), function (done) {
 		plugins.browserSync.reload();
 		done();
 	});
 	gulp.task('genNpm', function(callback) {
-		plugins.sequence(['npm:assets', 'npm:styles'],'styles:sass')(callback);
+		gulp.series(['npm:assets', 'npm:styles'],'styles:sass')(callback);
 	});
 	gulp.task('watchNpm', ['genNpm'], function (done) {
 		plugins.browserSync.reload();
 		done();
 	});
 
-  gulp.watch(options.scripts.src, ['watchScripts']);
-  gulp.watch(options.stealTools.devBundleWatch, ['watchVendorScripts']);
+  gulp.watch(options.scripts.src, gulp.series('watchScripts'));
+  gulp.watch(options.stealTools.devBundleWatch, gulp.series('watchVendorScripts'));
 
-	gulp.watch(path.join(options.styles.componentsSrc + '/**/*.scss'), ['styles:sass-index']);
-	gulp.watch(["!" + options.styles.srcFolder + options.npm.stylesFile, path.join(options.styles.srcFolder, '/**/*.{scss,sass}')], ['styles:sass', 'lint:styles']);
+	gulp.watch(path.join(options.styles.componentsSrc + '/**/*.scss'), gulp.series('styles:sass-index'));
+	gulp.watch(["!" + options.styles.srcFolder + options.npm.stylesFile, path.join(options.styles.srcFolder, '/**/*.{scss,sass}')], gulp.series('styles:sass', 'lint:styles'));
 
-	gulp.watch(options.fonts.src, ['watchFont']);
+	gulp.watch(options.fonts.src, gulp.series('watchFont'));
 
-	gulp.watch(options.npm.config, ['watchNpm']);
+	gulp.watch(options.npm.config, gulp.series('watchNpm'));
 
-	gulp.watch(options.images.src, ['watchImages']);
+	gulp.watch(options.images.src, gulp.series('watchImages'));
 
 	gulp.watch(options.utilities.watchSrc).on('change', plugins.browserSync.reload);
 };
